1ece8cc18df02b3a6489afa82fdb79fb4a6f80d6
Lev Koliadich <lkolyadich@gmail.com>
2025-04-12 21:42:13 +0200

    Change format of config files. Simplify validation.
    Change .storage files, so that onle 1 storage entry is in 1 file.
    Create functions decodeStorage and decodeEntry, which read file, decode
    the appropriate structure, validate if needed and add it to the Map of
    this structure.

--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TupleSections #-}
 
 module Main (main) where
 
-import Control.Arrow (Arrow (arr, first, (***)), (>>>))
-import Control.Monad (forM, forM_)
-import Data.Composition ((.:.))
-import Data.Function (on)
-import Data.Functor ((<&>))
-import Data.List (sort)
-import Data.Maybe (mapMaybe)
-import Data.Text (Text)
+import Control.Monad (foldM, forM)
+import Control.Monad.IO.Class (MonadIO)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
 import System.Directory
-import System.FilePath (isExtensionOf, (</>))
+import System.FilePath (isExtensionOf, takeBaseName, (</>))
 import Text.Printf (printf)
-import Toml (TomlCodec, decodeFile, diwrap, list, text, textBy, (.=))
+import Toml ((.=))
+import qualified Toml
+
+type StorageMap = M.Map String Storage
+
+type EntryMap = M.Map String Entry
 
 data Storage = Storage
-  { storageName :: Text,
-    storagePath :: Text,
+  { storagePath :: T.Text,
     storageKey :: Secret
   }
   deriving (Show, Eq, Ord)
 
 data Entry = Entry
   { entryType :: BackupType,
-    entryFrom :: Text,
-    entryTo :: Text,
-    entrySuccessHooks :: [Text],
-    entryFailureHooks :: [Text]
+    entryFrom :: T.Text,
+    entryTo :: T.Text,
+    entrySuccessHooks :: [T.Text],
+    entryFailureHooks :: [T.Text]
   }
   deriving (Show)
 
 data BackupType = File | Directory | DockerVolume
   deriving (Show, Read, Enum, Bounded)
 
-newtype Secret = Secret Text deriving (Eq, Ord)
+newtype Secret = Secret T.Text deriving (Eq, Ord)
 
 instance Show Secret where
   show _ = "********"
 
-storageFileCodec :: TomlCodec [Storage]
-storageFileCodec = Toml.list storageCodec "storage"
-
-storageCodec :: TomlCodec Storage
+storageCodec :: Toml.TomlCodec Storage
 storageCodec =
   Storage
-    <$> Toml.text "name" .= storageName
-    <*> Toml.text "path" .= storagePath
+    <$> Toml.text "path" .= storagePath
     <*> Toml.diwrap (Toml.text "key") .= storageKey
 
-entryCodec :: TomlCodec Entry
+entryCodec :: Toml.TomlCodec Entry
 entryCodec =
   Entry
     <$> Toml.textBy showType parseType "type" .= entryType
@@ -59,16 +55,16 @@ entryCodec =
     <*> Toml.list (Toml.text "cmd") "hook.success" .= entrySuccessHooks
     <*> Toml.list (Toml.text "cmd") "hook.failure" .= entrySuccessHooks
 
-showType :: BackupType -> Text
+showType :: BackupType -> T.Text
 showType File = "file"
 showType Directory = "directory"
 showType DockerVolume = "volume"
 
-parseType :: Text -> Either Text BackupType
+parseType :: T.Text -> Either T.Text BackupType
 parseType "file" = Right File
 parseType "directory" = Right Directory
 parseType "volume" = Right DockerVolume
-parseType other = Left $ "Unsupported backup type: " <> other
+parseType other = Left $ "unsupported backup type: " <> other
 
 filesIn :: String -> FilePath -> IO [FilePath]
 filesIn ext dir = do
@@ -77,46 +73,27 @@ filesIn ext dir = do
   let paths = (dir </>) <$> files
   return paths
 
-readStorageFile :: FilePath -> IO [(Storage, FilePath)]
-readStorageFile file = do
-  contents <- Toml.decodeFile storageFileCodec file
-  return $ (,file) <$> contents
+decodeStorage :: (MonadIO m) => StorageMap -> FilePath -> m StorageMap
+decodeStorage storageMap file = do
+  storage <- Toml.decodeFile storageCodec file
+  let name = takeBaseName file
+  return $ M.insert name storage storageMap
 
-pairwise :: [a] -> [(a, a)]
-pairwise xs = zip xs $ tail xs
-
-filterMaybe :: (a -> Bool) -> a -> Maybe a
-filterMaybe f x
-  | f x = Just x
-  | otherwise = Nothing
-
-notUniqueWith :: (Ord a) => (a -> a -> Bool) -> [a] -> [(a, a)]
-notUniqueWith f xs = mapMaybe func pairs
-  where
-    pairs = pairwise $ sort xs
-    func = filterMaybe (uncurry f)
-
-failCollidingStorage :: Text -> FilePath -> FilePath -> IO ()
-failCollidingStorage = fail .:. printf "storage entries with the same name: %s in %s and %s"
-
-uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
-uncurry3 f (a, b, c) = f a b c
-
-to3 :: ((a, b), c) -> (a, b, c)
-to3 ((x, y), z) = (x, y, z)
+decodeEntry :: (MonadIO m, MonadFail m) => S.Set String -> EntryMap -> FilePath -> m EntryMap
+decodeEntry storageEntries entryMap file = do
+  entry <- Toml.decodeFile entryCodec file
+  let storage = T.unpack $ entryTo entry
+  if storage `S.member` storageEntries
+    then return $ M.insert (takeBaseName file) entry entryMap
+    else fail $ printf "storage with name %s was not configured" $ show storage
 
 main :: IO ()
 main = do
   storageFiles <- filesIn "storage" "configs"
   entryFiles <- filesIn "entry" "configs"
 
-  storage <- concat <$> forM storageFiles readStorageFile
-  entries <- forM entryFiles $ Toml.decodeFile entryCodec
+  storageMap <- foldM decodeStorage M.empty storageFiles
+  entries <- forM entryFiles $ decodeEntry (M.keysSet storageMap) M.empty
 
-  let repeating = notUniqueWith ((==) `on` (storageName . fst)) storage
-  let failInputs = repeating <&> ((first storageName *** snd) >>> arr to3)
-
-  forM_ failInputs $ uncurry3 failCollidingStorage
-
-  print storage
+  print storageMap
   print entries
