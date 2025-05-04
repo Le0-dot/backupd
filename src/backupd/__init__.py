07c064de8e31e529f42ff4ef5de60c5d47528f1f
Lev Koliadich <lkolyadich@gmail.com>
2025-05-04 18:18:51 +0200

    Move BackupJob into separate module

--- src/backupd/__init__.py
+++ src/backupd/__init__.py
@@ -2,30 +2,21 @@ from contextlib import asynccontextmanager
 from functools import partial
 from http import HTTPStatus
 from itertools import chain
-from typing import Self
 
 from fastapi import FastAPI, Response
-from prometheus_client import Counter, Gauge, Summary
 
+from backupd.backup import BackupJob
 from backupd.docker import (
     Client,
     Container,
-    ContainerCreate,
-    Mount,
     container_by_name,
     list_containers,
-    run_container,
 )
 from backupd.metrics import (
     APIMetricsMiddleware,
-    backup_failure,
-    backup_success,
-    backup_time,
-    jobs,
     metrics_lifespan,
 )
 from backupd.repository import Repository
-from backupd.settings import Settings
 from backupd.task_queue import AppQueue, queue_lifespan
 
 
@@ -52,53 +43,6 @@ async def get_container(
 @app.get("/containers")
 async def get_containers(client: Client) -> list[Container]:
     return await list_containers(client)
-
-
-class BackupJob:
-    def __init__(
-        self, container_name: str, volume: str, repository: Repository
-    ) -> None:
-        self.config: ContainerCreate = ContainerCreate.shell(
-            image=Settings().runner_image,
-            cmd=f"{repository.preexec} &&"
-            + "echo {container} {volume}",  # NOTE: For testing purposes
-            # cmd=f"{repository.preexec} && restic check",
-            # cmd=f"{repository.preexec} && backup /data --tag backupd:{container}:{volume}",
-            env=repository.env,
-            mounts=[
-                repository.mount,
-                Mount(Target="/data", Source=volume, Type="volume", ReadOnly=True),
-            ],
-        )
-
-        self.job: Gauge = jobs.labels(
-            job_type="backup", container=container_name, volume=volume
-        )
-        self.success: Counter = backup_success.labels(
-            container=container_name, volume=volume
-        )
-        self.failure: Counter = backup_failure.labels(
-            container=container_name, volume=volume
-        )
-        self.time: Summary = backup_time.labels(container=container_name, volume=volume)
-
-        self.job.inc()
-
-    async def __call__(self) -> None:
-        with self.time.time():
-            result = await run_container(self.config, "backup")
-
-        self.job.dec()
-
-        counter = self.success if result.success else self.failure
-        counter.inc()
-
-        print(result.stdout)
-        print(result.stderr)
-
-    @classmethod
-    def for_container(cls, container: Container, repository: Repository) -> list[Self]:
-        return [cls(container.name, volume, repository) for volume in container.volumes]
 
 
 @app.post("/backup/{name}")
---
+++ src/backupd/backup.py
@@ -0,0 +1,76 @@
+from collections.abc import Callable
+from functools import partial
+from timeit import default_timer
+from typing import Self
+
+from prometheus_client import Counter, Gauge, Histogram
+
+from backupd.docker import (
+    Container,
+    ContainerCreate,
+    Mount,
+    run_container,
+)
+from backupd.metrics import (
+    backup_duration,
+    backup_result,
+    backup_start,
+    job_state,
+)
+from backupd.repository import Repository
+from backupd.settings import Settings
+
+
+class BackupJob:
+    def __init__(
+        self, container_name: str, volume: str, repository: Repository
+    ) -> None:
+        self.config: ContainerCreate = ContainerCreate.shell(
+            image=Settings().runner_image,
+            cmd=f"{repository.preexec} &&"
+            + f"echo {container_name} {volume}",  # NOTE: For testing purposes
+            # cmd=f"{repository.preexec} && restic check",
+            # cmd=f"{repository.preexec} && backup /data --tag backupd:{container}:{volume}",
+            env=repository.env,
+            mounts=[
+                repository.mount,
+                Mount(Target="/data", Source=volume, Type="volume", ReadOnly=True),
+            ],
+        )
+
+        self.start: Gauge = backup_start.labels(container=container_name, volume=volume)
+        self.result: partial[Counter] = partial(
+            backup_result.labels, container=container_name, volume=volume
+        )
+        self.duration: partial[Histogram] = partial(
+            backup_duration.labels, container=container_name, volume=volume
+        )
+        self.job: partial[Gauge] = partial(job_state.labels, kind="backup")
+
+        self.job(state="created").inc()
+
+    async def __call__(self) -> None:
+        self.job(state="created").dec()
+        self.job(state="started").inc()
+
+        start_time = default_timer()
+        result = await run_container(self.config, "backup")
+        finish_time = default_timer()
+
+        run_time = max(finish_time - start_time, 0)
+
+        self.job(state="started").dec()
+        self.job(state="finished").inc()
+
+        status = {True: "success", False: "failure"}[result.success]
+
+        self.start.set(start_time)
+        self.result(status=status).inc()
+        self.duration(status=status).observe(run_time)
+
+        print(result.stdout)
+        print(result.stderr)
+
+    @classmethod
+    def for_container(cls, container: Container, repository: Repository) -> list[Self]:
+        return [cls(container.name, volume, repository) for volume in container.volumes]
