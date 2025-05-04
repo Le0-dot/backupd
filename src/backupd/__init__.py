e4eab425d2a3ac22fbf031550054fd3093b89518
Lev Koliadich <lkolyadich@gmail.com>
2025-05-04 17:02:51 +0200

    Add BackupJob to encapsulate creation of backup tasks

--- src/backupd/__init__.py
+++ src/backupd/__init__.py
@@ -1,8 +1,11 @@
 from contextlib import asynccontextmanager
+from functools import partial
 from http import HTTPStatus
 from itertools import chain
+from typing import Self
 
 from fastapi import FastAPI, Response
+from prometheus_client import Counter, Gauge, Summary
 
 from backupd.docker import (
     Client,
@@ -14,9 +17,11 @@ from backupd.docker import (
     run_container,
 )
 from backupd.metrics import (
-    AppMetrics,
     APIMetricsMiddleware,
-    instrument_backup,
+    backup_failure,
+    backup_success,
+    backup_time,
+    jobs,
     metrics_lifespan,
 )
 from backupd.repository import Repository
@@ -49,21 +54,51 @@ async def get_containers(client: Client) -> list[Container]:
     return await list_containers(client)
 
 
-def configure_backup(
-    container: str, volume: str, repository: Repository
-) -> ContainerCreate:
-    backup_dir = "/data"
-    return ContainerCreate.shell(
-        image=Settings().runner_image,
-        cmd=f"{repository.preexec} &&" + "echo 123",  # NOTE: For testing purposes
-        # cmd=f"{repository.preexec} && restic check",
-        # cmd=f"{repository.preexec} && backup {backup_dir} --tag backupd:{container}:{volume}",
-        env=repository.env,
-        mounts=[
-            repository.mount,
-            Mount(Target=backup_dir, Source=volume, Type="volume", ReadOnly=True),
-        ],
-    )
+class BackupJob:
+    def __init__(
+        self, container_name: str, volume: str, repository: Repository
+    ) -> None:
+        self.config: ContainerCreate = ContainerCreate.shell(
+            image=Settings().runner_image,
+            cmd=f"{repository.preexec} &&"
+            + "echo {container} {volume}",  # NOTE: For testing purposes
+            # cmd=f"{repository.preexec} && restic check",
+            # cmd=f"{repository.preexec} && backup /data --tag backupd:{container}:{volume}",
+            env=repository.env,
+            mounts=[
+                repository.mount,
+                Mount(Target="/data", Source=volume, Type="volume", ReadOnly=True),
+            ],
+        )
+
+        self.job: Gauge = jobs.labels(
+            job_type="backup", container=container_name, volume=volume
+        )
+        self.success: Counter = backup_success.labels(
+            container=container_name, volume=volume
+        )
+        self.failure: Counter = backup_failure.labels(
+            container=container_name, volume=volume
+        )
+        self.time: Summary = backup_time.labels(container=container_name, volume=volume)
+
+        self.job.inc()
+
+    async def __call__(self) -> None:
+        with self.time.time():
+            result = await run_container(self.config, "backup")
+
+        self.job.dec()
+
+        counter = self.success if result.success else self.failure
+        counter.inc()
+
+        print(result.stdout)
+        print(result.stderr)
+
+    @classmethod
+    def for_container(cls, container: Container, repository: Repository) -> list[Self]:
+        return [cls(container.name, volume, repository) for volume in container.volumes]
 
 
 @app.post("/backup/{name}")
@@ -73,35 +108,27 @@ async def post_backup_container(
     response: Response,
     client: Client,
     queue: AppQueue,
-    metrics: AppMetrics,
 ) -> Container | None:
     container = await container_by_name(client, name)
     if container is None:
         response.status_code = HTTPStatus.NOT_FOUND
         return
 
-    for volume in container.volumes:
-        backup = instrument_backup(
-            metrics, run_container, container=name, volume=volume
-        )
-        configuration = configure_backup(name, volume, repository)
-        await queue.put(backup, configuration, "backup")
+    jobs = BackupJob.for_container(container, repository)
+    await queue.put(*jobs)
 
     return container
 
 
 @app.post("/backup")
 async def post_backup(
-    repository: Repository, client: Client, queue: AppQueue, metrics: AppMetrics
+    repository: Repository,
+    client: Client,
+    queue: AppQueue,
 ) -> list[Container]:
     containers = await list_containers(client)
-    containers_info = chain.from_iterable(map(Container.iter_volumes, containers))
 
-    for container, volume in containers_info:
-        backup = instrument_backup(
-            metrics, run_container, container=container, volume=volume
-        )
-        configuration = configure_backup(container, volume, repository)
-        await queue.put(backup, configuration, "backup")
+    jobs = map(partial(BackupJob.for_container, repository=repository), containers)
+    await queue.put(*chain.from_iterable(jobs))
 
     return containers
